299f4f728ad55f80fe63b179582079b72d640c5c
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2016-10-18 14:50:44 +0000

    scale_factor support for rester patterns, refactoring of raster symbolizers

--- include/mapnik/renderer_common/render_pattern.hpp
+++ include/mapnik/renderer_common/render_pattern.hpp
@@ -24,7 +24,9 @@
 #define MAPNIK_RENDER_PATTERN_HPP
 
 #include <mapnik/image.hpp>
-#include <memory>
+#include <mapnik/marker.hpp>
+#include <mapnik/symbolizer.hpp>
+#include <mapnik/renderer_common.hpp>
 
 // fwd decl
 namespace agg {
@@ -35,16 +37,67 @@ namespace mapnik {
 
 // fwd decl
 struct rasterizer;
-struct marker_svg;
+struct feature_impl;
 
-template <typename T>
-void render_pattern(rasterizer & ras,
+image_rgba8 render_pattern(rasterizer & ras,
                     marker_svg const& marker,
-                    agg::trans_affine const& tr,
-                    double opacity,
-                    T & image);
+                    box2d<double> const & bbox,
+                    agg::trans_affine const& tr);
+
+image_rgba8 render_pattern(rasterizer & ras,
+                    marker_rgba8 const& marker,
+                    box2d<double> const & bbox,
+                    agg::trans_affine const& tr);
+
+template <typename Symbolizer, typename Rasterizer>
+struct common_pattern_process_visitor
+{
+    using image_type = image_rgba8;
+
+    common_pattern_process_visitor(Rasterizer & ras,
+                                   renderer_common const & common,
+                                   Symbolizer const & sym,
+                                   feature_impl const & feature)
+        : ras_(ras),
+          common_(common),
+          sym_(sym),
+          feature_(feature)
+    {
+    }
+
+    image_type operator() (marker_null const &) const
+    {
+        throw std::runtime_error("This should not have been reached.");
+    }
+
+    template <typename Marker>
+    image_type operator() (Marker const & marker) const
+    {
+        box2d<double> bbox(marker.bounding_box());
+        agg::trans_affine tr(transform(bbox));
+        return render_pattern(ras_, marker, bbox, tr);
+    }
+
+private:
+    agg::trans_affine transform(box2d<double> & bbox) const
+    {
+        agg::trans_affine tr = agg::trans_affine_scaling(common_.scale_factor_);
+        auto image_transform = get_optional<transform_type>(sym_, keys::image_transform);
+        if (image_transform) evaluate_transform(tr, feature_, common_.vars_, *image_transform, common_.scale_factor_);
+        bbox *= tr;
+        coord<double, 2> c = bbox.center();
+        agg::trans_affine mtx = agg::trans_affine_translation(
+            0.5 * bbox.width() - c.x,
+            0.5 * bbox.height() - c.y);
+        return tr * mtx;
+    }
+
+    Rasterizer & ras_;
+    renderer_common const & common_;
+    Symbolizer const & sym_;
+    feature_impl const & feature_;
+};
 
 } // namespace mapnik
-
 
 #endif // MAPNIK_RENDER_PATTERN_HPP
