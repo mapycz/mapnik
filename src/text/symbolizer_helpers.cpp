a8d32d2c89072daaee8a5997d0a4ed1d3461c0b8
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2017-10-08 14:32:14 +0000

    find placements after reprojection

--- src/text/symbolizer_helpers.cpp
+++ src/text/symbolizer_helpers.cpp
@@ -41,6 +41,10 @@
 #include <mapnik/text/placement_finder_impl.hpp>
 #include <mapnik/text/placements/base.hpp>
 #include <mapnik/text/placements/dummy.hpp>
+#include <mapnik/geometry/transform.hpp>
+#include <mapnik/geometry/strategy.hpp>
+#include <mapnik/proj_strategy.hpp>
+#include <mapnik/view_strategy.hpp>
 
 namespace mapnik {
 namespace geometry {
@@ -287,20 +291,30 @@ void base_symbolizer_helper::initialize_points() const
                     success = true;
                 }
             }
-            else if (how_placed == INTERIOR_PLACEMENT && type == geometry::geometry_types::Polygon)
+            else if (type == geometry::geometry_types::Polygon)
             {
-                auto const& poly = mapnik::util::get<geometry::polygon<double> >(geom);
-                geometry::polygon_vertex_adapter<double> va(poly);
-                success = label::interior_position(va, label_x, label_y);
-            }
-            else if (how_placed == POLYLABEL_PLACEMENT && type == geometry::geometry_types::Polygon)
-            {
-                auto const& poly = mapnik::util::get<geometry::polygon<double>>(geom);
-                double precision = geometry::polylabel_precision(poly);
-                geometry::point<double> pt = geometry::polylabel(poly, precision);
-                label_x = pt.x;
-                label_y = pt.y;
-                success = true;
+                auto const& poly = util::get<geometry::polygon<double>>(geom);
+                proj_transform backwart_transform(prj_trans_.dest(), prj_trans_.source());
+                view_strategy vs(t_);
+                proj_strategy ps(backwart_transform);
+                using transform_group_type = geometry::strategy_group<proj_strategy, view_strategy>;
+                transform_group_type transform_group(ps, vs);
+                geometry::polygon<double> tranformed_poly(geometry::transform<double>(poly, transform_group));
+                if (how_placed == INTERIOR_PLACEMENT)
+                {
+                    geometry::polygon_vertex_adapter<double> va(tranformed_poly);
+                    if (label::interior_position(va, label_x, label_y))
+                    {
+                        points_.emplace_back(label_x, label_y);
+                    }
+                }
+                else if (how_placed == POLYLABEL_PLACEMENT)
+                {
+                    double precision = geometry::polylabel_precision(tranformed_poly);
+                    geometry::point<double> pt = geometry::polylabel(tranformed_poly, precision);
+                    points_.emplace_back(pt.x, pt.y);
+                }
+                continue;
             }
             else
             {
