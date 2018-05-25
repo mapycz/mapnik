bc5728e6fc199847f149b765695fa4199095a734
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2018-05-25 13:44:42 +0200

    w

--- include/mapnik/cairo/render_pattern.hpp
+++ include/mapnik/cairo/render_pattern.hpp
@@ -28,25 +28,9 @@
 #include <mapnik/symbolizer.hpp>
 #include <mapnik/renderer_common.hpp>
 #include <mapnik/svg/svg_converter.hpp>
-#include <mapnik/svg/svg_renderer_agg.hpp>
 #include <mapnik/svg/svg_path_adapter.hpp>
-#include <mapnik/agg_rasterizer.hpp>
-#include <mapnik/util/const_rendering_buffer.hpp>
 #include <mapnik/marker_helpers.hpp>
 #include <mapnik/cairo/cairo_context.hpp>
-
-#pragma GCC diagnostic push
-#include <mapnik/warning_ignore_agg.hpp>
-#include "agg_rendering_buffer.h"
-#include "agg_pixfmt_rgba.h"
-#include "agg_pixfmt_gray.h"
-#include "agg_color_rgba.h"
-#include "agg_color_gray.h"
-#include "agg_scanline_u.h"
-#include "agg_image_accessors.h"
-#include "agg_span_image_filter_rgba.h"
-#include "agg_rasterizer_scanline_aa.h"
-#pragma GCC diagnostic pop
 
 namespace mapnik {
 
@@ -82,12 +66,12 @@ struct cairo_common_pattern_process_visitor
 
         if (lacing_ == PATTERN_LACING_MODE_ALTERNATING_GRID)
         {
-            //return render_pattern_alternating(ras_, marker, bbox, tr);
+            return render_pattern_alternating(marker, bbox, tr);
         }
         else
         {
-        }
             return render_pattern(marker, bbox, tr);
+        }
     }
 
 private:
@@ -151,7 +135,8 @@ private:
         svg_attribute_type const & svg_attributes = svg.attributes();
         svg_attribute_type custom_attributes;
         bool use_custom_attributes = push_explicit_style(
-            svg_attributes, custom_attributes, sym_, feature_, common_.vars_);
+            svg_attributes, custom_attributes, sym_,
+            feature_, common_.vars_);
         svg_attribute_type const & used_attributes =
             use_custom_attributes ?  custom_attributes : svg_attributes;
 
@@ -172,8 +157,8 @@ private:
                                agg::trans_affine tr) const
     {
         cairo_rectangle_t extent { 0, 0,
-            std::floor(bbox.width() + spacing_x_),
-            std::floor(bbox.height() + spacing_y_) };
+            bbox.width() + spacing_x_,
+            bbox.height() + spacing_y_ };
         cairo_surface_ptr surface(
             cairo_recording_surface_create(
                 CAIRO_CONTENT_COLOR_ALPHA, &extent),
@@ -189,189 +174,87 @@ private:
         return surface;
     }
 
-/*
-            int width = std::max(1, static_cast<int>(
-                bbox.width() + spacing_x_)),
-            int height = std::max(1, static_cast<int>(
-                bbox.height() + spacing_y_)));
-
-        image_rgba8 image(std::max(1, static_cast<int>(bbox.width() + spacing_x_)),
-                          std::max(1, static_cast<int>(bbox.height() + spacing_y_)));
-        agg::rendering_buffer buf(image.bytes(), image.width(), image.height(), image.row_size());
-        pixfmt pixf(buf);
-        renderer_base renb(pixf);
-
-        svg_storage_type & svg = *marker.get_data();
-        svg_attribute_type const & svg_attributes = svg.attributes();
-        svg_attribute_type custom_attributes;
-        bool use_custom_attributes = push_explicit_style(
-            svg_attributes, custom_attributes, sym_, feature_, common_.vars_);
-        svg_attribute_type const & used_attributes = use_custom_attributes ?
-            custom_attributes : svg_attributes;
-
-        svg::vertex_stl_adapter<svg::svg_path_storage> stl_storage(svg.source());
-        svg::svg_path_adapter svg_path(stl_storage);
-        using renderer_type = svg::svg_renderer_agg<svg::svg_path_adapter,
-            agg::pod_bvector<svg::path_attributes>, renderer_solid, pixfmt>;
-        renderer_type svg_renderer(svg_path, used_attributes);
-
-        tr.translate(spacing_x_ / 2.0, spacing_y_ / 2.0);
-        svg_renderer.render(ras, sl, renb, tr, 1.0, bbox);
-        return image;
-    }
-
-    image_rgba8 render_pattern(rasterizer & ras,
-                               marker_rgba8 const& marker,
-                               box2d<double> const & bbox,
-                               agg::trans_affine tr) const
-    {
-        using pixfmt = agg::pixfmt_rgba32_pre;
-        using renderer_base = agg::renderer_base<pixfmt>;
-        using renderer = agg::renderer_scanline_aa_solid<renderer_base>;
-
-        image_rgba8 image(bbox.width() + spacing_x_, bbox.height() + spacing_y_);
-        agg::rendering_buffer buf_out(image.bytes(), image.width(), image.height(), image.row_size());
-        pixfmt pixf_out(buf_out);
-        renderer_base rb(pixf_out);
-        renderer r(rb);
-
-        using const_rendering_buffer = util::rendering_buffer<image_rgba8>;
-        using pixfmt_in = agg::pixfmt_alpha_blend_rgba<agg::blender_rgba32_pre, const_rendering_buffer, agg::pixel32_type>;
-
-        image_rgba8 const& src = marker.get_data();
-        const_rendering_buffer buf_in(src);
-        pixfmt_in pixf(buf_in);
-
-        tr.translate(spacing_x_ / 2.0, spacing_y_ / 2.0);
-        tr.invert();
-
-        using interpolator_type = agg::span_interpolator_linear<>;
-        interpolator_type interpolator(tr);
-
-        agg::span_allocator<agg::rgba8> sa;
-
-        agg::image_filter_lut filter;
-        filter.calculate(agg::image_filter_bilinear(), true);
-
-        using img_accessor_type = agg::image_accessor_clone<pixfmt_in>;
-        using span_gen_type = agg::span_image_resample_rgba_affine<img_accessor_type>;
-        img_accessor_type ia(pixf);
-        span_gen_type sg(ia, interpolator, filter);
-
-        agg::scanline_u8 sl;
-        ras.move_to_d(0, 0);
-        ras.line_to_d(image.width(), 0);
-        ras.line_to_d(image.width(), image.height());
-        ras.line_to_d(0, image.height());
-
-        agg::render_scanlines_aa(ras, sl, rb, sa, sg);
-
-        return image;
-    }
-
-    image_rgba8 render_pattern_alternating(rasterizer & ras,
+    cairo_surface_ptr render_pattern_alternating(
                                marker_svg const & marker,
                                box2d<double> const & bbox,
                                agg::trans_affine tr) const
     {
-        using pixfmt = agg::pixfmt_rgba32_pre;
-        using renderer_base = agg::renderer_base<pixfmt>;
-        using renderer_solid = agg::renderer_scanline_aa_solid<renderer_base>;
-        agg::scanline_u8 sl;
+        // The std::floor should prevent from unexpected spaces
+        // between pattern tiles in cases when the bbox is not precise.
+        cairo_rectangle_t extent { 0, 0,
+            std::floor(bbox.width() + spacing_x_),
+            std::floor(bbox.height() + spacing_y_) * 2.0 };
+        cairo_surface_ptr surface(
+            cairo_recording_surface_create(
+                CAIRO_CONTENT_COLOR_ALPHA, &extent),
+            cairo_surface_closer());
 
-        image_rgba8 image(
-            std::max(1, static_cast<int>(std::round(bbox.width() + spacing_x_))),
-            std::max(1, static_cast<int>(std::round((bbox.height() + spacing_y_) * 2.0))));
-        agg::rendering_buffer buf(image.bytes(), image.width(), image.height(), image.row_size());
-        pixfmt pixf(buf);
-        renderer_base renb(pixf);
+        cairo_ptr cairo = create_context(surface);
+        cairo_context context(cairo);
 
         svg_storage_type & svg = *marker.get_data();
         svg_attribute_type const & svg_attributes = svg.attributes();
         svg_attribute_type custom_attributes;
         bool use_custom_attributes = push_explicit_style(
-            svg_attributes, custom_attributes, sym_, feature_, common_.vars_);
-        svg_attribute_type const & used_attributes = use_custom_attributes ?
-            custom_attributes : svg_attributes;
+            svg_attributes, custom_attributes, sym_,
+            feature_, common_.vars_);
+        svg_attribute_type const & used_attributes =
+            use_custom_attributes ?  custom_attributes : svg_attributes;
 
-        svg::vertex_stl_adapter<svg::svg_path_storage> stl_storage(svg.source());
+        svg::vertex_stl_adapter<svg::svg_path_storage> stl_storage(
+            svg.source());
         svg::svg_path_adapter svg_path(stl_storage);
-        using renderer_type = svg::svg_renderer_agg<svg::svg_path_adapter,
-            agg::pod_bvector<svg::path_attributes>, renderer_solid, pixfmt>;
-        renderer_type svg_renderer(svg_path, used_attributes);
 
         tr.translate(spacing_x_ / 2.0, spacing_y_ / 2.0);
-        svg_renderer.render(ras, sl, renb, tr, 1.0, bbox);
+
+        render_vector_marker(context, svg_path, used_attributes,
+            bbox, tr, opacity_);
+
         tr.translate(std::round(-(bbox.width() / 2.0 + spacing_x_ / 2.0)),
             bbox.height() + spacing_y_);
-        svg_renderer.render(ras, sl, renb, tr, 1.0, bbox);
+
+        render_vector_marker(context, svg_path, used_attributes,
+            bbox, tr, opacity_);
+
         tr.translate(std::round(bbox.width() + spacing_x_), 0);
-        svg_renderer.render(ras, sl, renb, tr, 1.0, bbox);
-        return image;
+
+        render_vector_marker(context, svg_path, used_attributes,
+            bbox, tr, opacity_);
+
+        return surface;
     }
 
-    image_rgba8 render_pattern_alternating(rasterizer & ras,
+    cairo_surface_ptr render_pattern_alternating(
                                marker_rgba8 const& marker,
                                box2d<double> const & bbox,
                                agg::trans_affine tr) const
     {
-        using pixfmt = agg::pixfmt_rgba32_pre;
-        using renderer_base = agg::renderer_base<pixfmt>;
-        using renderer = agg::renderer_scanline_aa_solid<renderer_base>;
+        cairo_rectangle_t extent { 0, 0,
+            bbox.width() + spacing_x_,
+            bbox.height() + spacing_y_ * 2.0 };
+        cairo_surface_ptr surface(
+            cairo_recording_surface_create(
+                CAIRO_CONTENT_COLOR_ALPHA, &extent),
+            cairo_surface_closer());
 
-        image_rgba8 image(
-            std::round(bbox.width() + spacing_x_),
-            std::round((bbox.height() + spacing_y_) * 2.0));
-        agg::rendering_buffer buf_out(image.bytes(), image.width(), image.height(), image.row_size());
-        pixfmt pixf_out(buf_out);
-        renderer_base rb(pixf_out);
-        renderer r(rb);
-
-        using const_rendering_buffer = util::rendering_buffer<image_rgba8>;
-        using pixfmt_in = agg::pixfmt_alpha_blend_rgba<agg::blender_rgba32_pre, const_rendering_buffer, agg::pixel32_type>;
-
-        image_rgba8 const& src = marker.get_data();
-        const_rendering_buffer buf_in(src);
-        pixfmt_in pixf(buf_in);
+        cairo_ptr cairo = create_context(surface);
+        cairo_context context(cairo);
 
         tr.translate(spacing_x_ / 2.0, spacing_y_ / 2.0);
-        tr.invert();
 
-        using interpolator_type = agg::span_interpolator_linear<>;
-        interpolator_type interpolator(tr);
+        context.add_image(tr, marker.get_data(), opacity_);
 
-        agg::span_allocator<agg::rgba8> sa;
-
-        agg::image_filter_lut filter;
-        filter.calculate(agg::image_filter_bilinear(), true);
-
-        using img_accessor_type = agg::image_accessor_clone<pixfmt_in>;
-        using span_gen_type = agg::span_image_resample_rgba_affine<img_accessor_type>;
-        img_accessor_type ia(pixf);
-        span_gen_type sg(ia, interpolator, filter);
-
-        agg::scanline_u8 sl;
-        ras.move_to_d(0, 0);
-        ras.line_to_d(image.width(), 0);
-        ras.line_to_d(image.width(), image.height());
-        ras.line_to_d(0, image.height());
-
-        agg::render_scanlines_aa(ras, sl, rb, sa, sg);
-
-        tr.invert();
         tr.translate(std::round(-(bbox.width() / 2.0 + spacing_x_ / 2.0)),
             bbox.height() + spacing_y_);
-        tr.invert();
-        agg::render_scanlines_aa(ras, sl, rb, sa, sg);
 
-        tr.invert();
+        context.add_image(tr, marker.get_data(), opacity_);
+
         tr.translate(std::round(bbox.width() + spacing_x_), 0);
-        tr.invert();
-        agg::render_scanlines_aa(ras, sl, rb, sa, sg);
 
-        return image;
+        context.add_image(tr, marker.get_data(), opacity_);
+
+        return surface;
     }
-    */
 
     renderer_common const & common_;
     Symbolizer const & sym_;
