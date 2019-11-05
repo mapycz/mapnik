c5de69c86eb25df3fe68b783a1ffb3c91ae88524
Jiri Drbalek <jiri.drbalek@firma.seznam.cz>
2019-11-05 15:47:12 +0100

    Remove useless dependency

--- deps/boost/container_hash/hash_fwd.hpp
+++ deps/boost/container_hash/hash_fwd.hpp
@@ -10,7 +10,6 @@
 #if !defined(BOOST_FUNCTIONAL_HASH_FWD_HPP)
 #define BOOST_FUNCTIONAL_HASH_FWD_HPP
 
-#include <boost/config/workaround.hpp>
 #include <cstddef>
 
 #if defined(BOOST_HAS_PRAGMA_ONCE)
@@ -26,11 +25,6 @@ namespace boost
 
     template <class It> std::size_t hash_range(It, It);
     template <class It> void hash_range(std::size_t&, It, It);
-
-#if BOOST_WORKAROUND(__BORLANDC__, BOOST_TESTED_AT(0x551))
-    template <class T> inline std::size_t hash_range(T*, T*);
-    template <class T> inline void hash_range(std::size_t&, T*, T*);
-#endif
 }
 
 #endif
